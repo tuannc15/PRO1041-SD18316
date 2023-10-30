79343e88626c5cdd123cb8463ce38f97cc2656d7
nctuan2013 <54241848+nctuan2013@users.noreply.github.com>
2023-10-30 17:57:11 +0700

    Khoi tao GIT

---
+++ ChaoLopSD18316/src/chaolopsd18316/ChaoLopSD18316.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chaolopsd18316;
+
+/**
+ *
+ * @author nctuan
+ */
+public class ChaoLopSD18316 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Chào các bạn lớp SD18316");
+    }
+    
+}
